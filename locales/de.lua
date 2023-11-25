df90e3ef67bc77d65dddd4122ae44e95d19e36d4
Crizii <135024567+CriziiTV@users.noreply.github.com>
2023-11-25 23:32:09 +0100

    Create de.lua
    German Language File

---
+++ locales/de.lua
@@ -0,0 +1,15 @@
+Locales["de"] = {
+    toggleHud = 'HUD ein- / ausschalten',
+    indicatorLeft = 'Linker Blinker',
+    indicatorRight = 'Rechter Blinker',
+    toggleEngine = 'Motor ein / aus',
+    speedChanged = 'Geschwindigkeit geändert, neue Geschwindigkeit: ',
+    resource_not_built = 'Ressource ist nicht erstellt, bitte laden Sie die Release-Version herunter',
+    settingsSaveSuccess = "Aktuelle Einstellungen erfolgreich gespeichert",
+    settingsResetSuccess = "Einstellungen erfolgreich zurückgesetzt",
+    currentVersion = "Neueste Version: ",
+    yourVersion = "Ihre Version: ",
+    latestVersion = "Sie verwenden die neueste Version!",
+    needUpdateResource = "Sie müssen die neueste Version herunterladen! Sie verwenden derzeit eine alte Version!",
+    errorGetCurrentVersion = "Sie erhalten einen Fehler beim Versuch, die aktuelle Version zu erhalten!"
+}
